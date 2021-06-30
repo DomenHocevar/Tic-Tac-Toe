d5c6d796cb7a3e0acfdb6ad2ccc6a35ded39ef34
domen <domenhocevar1@gmail.com>
2021-06-30 20:51:56 +0200

    Connected the display and game logic, also added restart button

--- script.js
+++ script.js
@@ -17,6 +17,10 @@ const gameController = ((playerPlaying, playerWaiting) => {
         return null;
     }
 
+    function getCell(i, j) {
+        return gameBoard.getCell(i, j);
+    }
+
     /*
     Returns the player whose turn it is as the first and the other as the second in an array.
     */
@@ -71,10 +75,10 @@ const gameController = ((playerPlaying, playerWaiting) => {
             if (end && map[0][0] != null) return map[0][0];
     
             let end2 = true;
-            for (let i = 3 - 1, j = 0; i >= 0; i--, j++) {
-                if (map[i][j] != map[2][0]) end = false;
+            for (let i = 2, j = 0; i >= 0; i--, j++) {
+                if (map[i][j] != map[2][0]) end2 = false;
             }
-            if (end && map[2][0] != null) return map[2][0];
+            if (end2 && map[2][0] != null) return map[2][0];
     
             if (empty == 0) {
                 return "draw";
@@ -84,14 +88,15 @@ const gameController = ((playerPlaying, playerWaiting) => {
     
         function setCell(i, j, player) {
             if (map[i][j] == null) {
+                console.log(player);
                 map[i][j] = player;
                 return true;
             }
             return false;
         }
     
-        function getCell(params) {
-            map[i][j]
+        function getCell(i, j) {
+            return map[i][j];
         }
 
     
@@ -100,7 +105,7 @@ const gameController = ((playerPlaying, playerWaiting) => {
 
 
 
-    return {playCell, getOrder};
+    return {playCell, getOrder, getCell};
 });
 
 
@@ -116,7 +121,11 @@ const player = (isCircle, name) =>{
         score++;
     }
 
-    return {win, name, type};
+    function getScore(params) {
+        return score;
+    }
+
+    return {win, name, type, getScore};
 }
 
 const displayMenu = (() => {
@@ -147,7 +156,12 @@ const displayMenu = (() => {
             alert("Other player's name is empty!");
             return;
         }
-
+        /*
+        if (name1 == name2) {
+            alert("The names must not be the same!");
+            return;
+        }
+        */
 
         displayController.advanceToGame(player(true, name1), player(false, name2));
     }
@@ -159,15 +173,34 @@ const displayGame = (() => {
     const game = document.querySelector("#game");
     const grid = document.querySelector("#grid");
     const menuButton = document.querySelector("#menuButton");
-    menuButton.addEventListener("click", returnToMenu);
+    const restartButton = document.querySelector("#restartButton");
+    const messageSpan = document.querySelector("#messageSpan");
+    const scoreName1 = document.querySelector("#scoreName1");
+    const scoreName2 = document.querySelector("#scoreName2");
+    const scoreNumber1 = document.querySelector("#scoreNumber1");
+    const scoreNumber2 = document.querySelector("#scoreNumber2");
     
+    let player1;
+    let player2;
     let round;
-    let ended;
+
+    menuButton.addEventListener("click", returnToMenu);
+    restartButton.addEventListener("click", () => (init(player1, player2)));
 
     function init(nextPlayer1, nextPlayer2) {
-        round = gameController(nextPlayer1, nextPlayer2);
-        ended = false;
+        player1 = nextPlayer1;
+        player2 = nextPlayer2;
+        round = gameController(player1, player2);
 
+        scoreName1.textContent = player1.name;
+        scoreName2.textContent = player2.name; 
+
+        updateScore();
+        updateGrid();
+        setHasTurnMessage(); 
+    }
+
+    function updateGrid(params) {
         const childrenArray = Array.from(grid.childNodes);
         childrenArray.forEach(child => child.remove());
         for (let i = 0; i < 3; i++) {
@@ -176,13 +209,63 @@ const displayGame = (() => {
                 block.classList.add("block");
                 block.dataset.i = i;
                 block.dataset.j = j;
+                if (round.getCell(i, j) != null) {
+                    if (round.getCell(i, j).type === "circle") {
+                        block.textContent = "O";
+                        block.style.color = "blue";
+                    } else {
+                        block.textContent = "X";
+                        block.style.color = "red";
+                    }
+                }
+                block.addEventListener("click", playCell);
                 grid.appendChild(block);
             }
-        } 
+        }        
     }
 
-    function update(params) {
-        
+    function updateScore(params) {
+        scoreNumber1.textContent = player1.getScore();
+        scoreNumber2.textContent = player2.getScore();
+    }
+
+    function playCell() {
+        const i = this.dataset.i;
+        const j = this.dataset.j;
+        processResult(round.playCell(i, j));
+    }
+
+    function setHasTurnMessage(params) {
+        messageSpan.textContent = "It's " + round.getOrder()[0].name + "'s turn.";
+    }
+
+    function setHasWonMessage(player) {
+        messageSpan.textContent = "The winner is " + player.name + "!";
+    }
+
+    function setDrawMessage(params) {
+        messageSpan.textContent = "It's a draw!";
+    }
+
+    function processResult(result) {
+        console.log(result);
+        console.log(round.getCell(0, 0));
+        updateGrid();
+        if (result == null) return;
+        if (result === "continue") {
+            setHasTurnMessage();
+            return;
+        }
+
+        grid.childNodes.forEach(child => child.removeEventListener("click", playCell));
+
+        if (result == "draw") setDrawMessage();
+        else {
+            result.win();
+            setHasWonMessage(result);
+        }
+
+        updateScore();
     }
     
     function turnOn(params) {
